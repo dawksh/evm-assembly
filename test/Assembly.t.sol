89ccb0519a9e7d06260c4c112c2f8060d0ea2c8e
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2023-06-10 21:41:52 +0530

    feat: airdrop test

---
+++ test/Assembly.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Assembly.sol";
+
+contract AssemblyTest is Test {
+    Assembly public ass;
+    address[] internal adds = [
+        address(0xA93DFf7336fd200cE30Bc25F5BFE46b74D2248b5),
+        address(0x5e009E6c786Ce23eB95338b8740E66d243520D2f),
+        address(0xACB471904C26D1FFE1fD0AA88b75944C513af361)
+    ];
+    uint256[] internal amounts = [0.1 ether, 0.2 ether, 0.3 ether];
+
+    function setUp() public {
+        ass = new Assembly();
+    }
+
+    function testAirdrop() public {
+        ass.airdropETH{value: 0.6 ether}(adds, amounts);
+    }
+}
